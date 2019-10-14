cac0a0af76ec62d6c5e4f02cdd9524b711457399
EnzoAliatis <enzo.aliatis@gmail.com>
2019-10-14 18:34:45 -0500

    Listo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [string, setString] = useState('')
+  const [code, setCode] = useState('')
+  const [response, setResponse] = useState({})
+
+
+  const getMatch = () => {
+    fetch('http://0.0.0.0:5000/subtitles', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        string,
+        code
+      })
+    }).then(res => console.log(res.json().then(json => {
+      console.log(json)
+      setResponse(json)
+    })))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      <h1>Prueba WEFERE</h1>
+      <div className='input-group mb-3'>
+        <div className='input-group-prepend'>
+          <span className='input-group-text' id='basic-addon1'>
+            STRING
+          </span>
+        </div>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Lavense la carita'
+          onChange={e => {
+            setString(e.target.value)
+          }}
+        />
+      </div>
+      <div className='input-group mb-3'>
+        <div className='input-group-prepend'>
+          <span className='input-group-text' id='basic-addon1'>
+            Codigo del video
+          </span>
+        </div>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='MvmcjwVBd9c'
+          onChange={e => {
+            setCode(e.target.value)
+          }}
+        />
+      </div>
+      <button
+        type='button'
+        className='btn btn-primary'
+        onClick={() => getMatch()}>
+        Get Match
+      </button>
+
+      <h4>Respuesta</h4>
+      <p>Start: {response.start}</p>
+      <p>dur: {response.dur}</p>
+      <p>text: {response.text}</p>
+      <p>Timestamp: {response.Timestamp}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+// Xgaz7dzyDKU
+// ese tema y ojalá no le impulsen
